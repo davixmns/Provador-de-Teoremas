c9b4f44663bb7296cf3f5e50decba23923af5bc6
Tarcizo Neto <tarcizon12@gmail.com>
2022-05-30 21:44:23 -0300

    Refatorando

--- src/Tarcizo/Analisador.java
+++ src/Tarcizo/Analisador.java
@@ -7,21 +7,16 @@ import java.util.List;
 public class Analisador { //ANALISADOR LÉXICO
     List<String> listaDeSimbolos = List.of("~", "^", "v", "➝", "↔");
 
-    public Boolean verificaExistencia(String recebido, List<String> lista) {
-        return lista.contains(recebido);
-    }
-
     public String contabilizaInvalidos(String simbolos) {
-        String[] simbolosRecebidos = simbolos.split("(?!^)");
+
+        String[] simbolosRecebidos = simbolos.split("(?!^)");;
         List<String> simbolosInvalidos = new ArrayList<>();
 
         for (String i : simbolosRecebidos) {
-            if (!verificaExistencia(i, listaDeSimbolos)) {
-                if (!verificaExistencia(i, simbolosInvalidos)) {
-                    simbolosInvalidos.add(i);
-                }
-            }
+            if(!listaDeSimbolos.contains(i)) simbolosInvalidos.add(i);
         }
+
         return "Os simbolos invalidos sao: " + simbolosInvalidos;
     }
+
 }
--- src/Tarcizo/Teste.java
+++ src/Tarcizo/Teste.java
@@ -3,14 +3,14 @@ package Tarcizo;
 
 public class Teste {
     public static void main(String[] args) {
-//        Analisador analisador = new Analisador();
+        Analisador analisador = new Analisador();
         FBF verificaFBF = new FBF();
 
-//        String invalidos = "~aDlq.ca➝↔";
+       String invalidos = "~aDlq.ca➝↔";
 
         String formula = " c ➝ (a ^ b) ";
 
-//        System.out.println(analisador.contabilizaInvalidos(invalidos));
+       System.out.println(analisador.contabilizaInvalidos(invalidos));
         System.out.println(verificaFBF.verificaFbf(formula)); //  Verificador de fbf
 
     }
